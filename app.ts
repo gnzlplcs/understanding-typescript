c64643db1f13fa2bd0fa36ce28c134542f2b74ca
gnzlplcs <palaciosgm@outlook.com>
2023-01-11 16:49:19 -0500

    :construction: working w/numbers, strings & booleans
    lesson 14

--- app.ts
+++ app.ts
@@ -1,12 +1,17 @@
-const add = (n1: number, n2: number) => {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
   //   throw new Error('Incorrect input')
   // }
-  return n1 + n2
+  const result = n1 + n2
+  if (showResult) {
+    console.log(phrase + result)
+  }
+  return result
 }
 
-const number1 = '5'
+const number1 = 5
 const number2 = 2.8
+const printResult = true
+const resultPhrase = 'Result is: '
 
-const result = add(number1, number2)
-console.log(result)
+add(number1, number2, printResult, resultPhrase)
