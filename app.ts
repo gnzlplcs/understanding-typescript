a4dd91126036148eba71a98ff39f0fba8c0c7ca8
gnzlplcs <palaciosgm@outlook.com>
2023-01-11 16:40:05 -0500

    :construction: TypeScript types vs JavaScript types
    lesson 12

--- app.ts
+++ app.ts
@@ -1,8 +1,11 @@
 const add = (n1: number, n2: number) => {
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //   throw new Error('Incorrect input')
+  // }
   return n1 + n2
 }
 
-const number1 = 5
+const number1 = '5'
 const number2 = 2.8
 
 const result = add(number1, number2)
